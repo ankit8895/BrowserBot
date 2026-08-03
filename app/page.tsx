7f6ed02a0ceac801f2e98ccef1f939c5464d5f45
ankit8895 <ankit9185731@gmail.com>
2026-08-03 20:47:56 +0530

    authentication setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import React from "react";
+import { toast } from "sonner";
 
 const page = () => {
-  return <div>page</div>;
+  return <div>Home</div>;
 };
 
 export default page;
